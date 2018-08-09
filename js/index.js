dfbdb37b62844ebe1105016b9c8edbad6f1dfb76
Garrus Napp <garrusnapp@gmail.com>
2018-08-09 14:42:01 +0200

    basic functionality of mobile menu

--- js/index.js
+++ js/index.js
@@ -1,5 +1,18 @@
-import "../sass/main.scss"
+import "../sass/main.scss";
 
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("hello");
+  const hamburgerButton = document.getElementById("hamburger-button");
+  const listSmall = document.getElementsByClassName("link-list-small")[0];
+  const smallMenuTexts = document.getElementsByClassName("small-menu-text");
+
+  hamburgerButton.addEventListener("click", function(e) {
+    listSmall.classList.toggle("list-visible");
+  });
+
+  [...smallMenuTexts].forEach(el =>
+    el.addEventListener("click", function(e) {
+      console.log(e);
+      e.stopPropagation();
+    })
+  );
 });
